ced07ebe586cabbb55345479412aefb0e59e15f4
Pedro Ailton <pedroailton630@gmail.com>
2025-09-27 21:48:14 -0300

    Update busca-binaria.py

--- aula-4-20250925/busca-binaria.py
+++ aula-4-20250925/busca-binaria.py
@@ -1,28 +1,23 @@
-from math import floor
-
 def buscaBinaria(elemento, lista, qtd_comparacoes=0):
     if len(lista) == 0: # Caso base: Lista vazia (não faz mais comparações)
         return qtd_comparacoes
     else:
-        if len(lista) % 2 == 0: # Se o tamanho da lista for par
-            meio = len(lista)/2
-        else: # Se o tamanho da lista for ímpar
-            meio = floor(len(lista)/2)
-        if meio == elemento:
+        meio = len(lista)//2
+        if lista[meio] == elemento:
             qtd_comparacoes += 1
             return qtd_comparacoes
-        elif meio < elemento: # O elemento está à direita
+        elif lista[meio] < elemento: # O elemento está à direita
             qtd_comparacoes += 1
             return buscaBinaria(elemento, lista[meio+1:], qtd_comparacoes)
-        elif meio > elemento: # O elemento está à esquerda
+        elif lista[meio] > elemento: # O elemento está à esquerda
             qtd_comparacoes += 1
-            return buscaBinaria(elemento, lista[0:meio-1], qtd_comparacoes)
+            return buscaBinaria(elemento, lista[:meio], qtd_comparacoes)
 
 elemento = 0
 lista = []
 resultado = 0
 
-entrada = input("")
+entrada = input("").strip()
 elementos_da_entrada = entrada.split(" ")
 
 for i in elementos_da_entrada:
